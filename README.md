38dbec50f026f8acbfcb6ab6ec9ec7468cc5614e
Vootele Verrev <verrev1@hotmail.com>
2015-03-18 00:42:23 +0200

    restarting

--- README.md
+++
@@ -1,2 +0,0 @@
-# fbx2vmf
-Converts .fbx files to .vmf files.
--- fbx2vmf/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "CConverter.h"
-int main(int argc, char *argv[])
-{
-	if (argc > 1){
-		CConverter c;
-		c.convertScene(std::string(argv[1]));
-	}
-}
